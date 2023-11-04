2416d9cbdf237dca2a29045ef5ad6e01a66a2690
WangNannan <blukis@foxmail.com>
2023-11-04 19:50:47 +0800

    fix spelling error and clean code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
+## 1.0.2
+* chore - fix spelling errors
+* chore - clean code
+
 ## 1.0.1
 
-* use default dart format configirutaion
+* use default dart format configuration
 
 ## 1.0.0
 
--- lib/rendering/render_sliver_sticky_collapsable_panel.dart
+++ lib/rendering/render_sliver_sticky_collapsable_panel.dart
@@ -64,7 +64,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
 
   RenderBox? _header;
 
-  /// The render object's header
   RenderBox? get headerChild => _header;
 
   set headerChild(RenderBox? value) {
@@ -75,7 +74,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
 
   RenderSliver? _sliver;
 
-  /// The render object's unique child
   RenderSliver? get sliverChild => _sliver;
 
   set sliverChild(RenderSliver? value) {
@@ -142,13 +140,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
 
   double get headerLogicalExtent => overlapsContent ? 0 : _headerExtent;
 
-  //为什么我们不担心会调用performResize，因为RenderSliver类，均不允许sizedByParent
-  //想想也是，Sliver都是放在某个纬度无限的Parent里面的，自然不能约束Sliver
-  // @override
-  // void performResize() {
-  //   super.performResize();
-  // }
-
   @override
   void performLayout() {
     if (headerChild == null && sliverChild == null) {
@@ -156,13 +147,10 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
       return;
     }
 
-    // One of them is not null.
     AxisDirection axisDirection = applyGrowthDirectionToAxisDirection(
         constraints.axisDirection, constraints.growthDirection);
 
     if (headerChild != null) {
-      // 这里先使用_oldStatus来layout，后续会再重试的
-      // 这里只是利用RenderBox和constraints来获取header的extent（宽或者高）
       headerChild!.layout(
         BoxValueConstraints<SliverStickyCollapsablePanelStatus>(
           value: _oldStatus ??
@@ -174,16 +162,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
       _headerExtent = computeHeaderExtent();
     }
 
-    // Compute the header extent only one time.
-    // 我们假设header高度50，sliverChild高度100，view port 500，scroll offset 50
-    // 我们可以计算出headerPaintExtent是0
-    // 我们可以计算出headerCacheExtent是50，
-    // 再次假设我们scr0ll offset 是300
-    //  那么因为remainingPaintExtent是500，所以计算出的headerPaintExtent是0
-    //  那么因为cacheOrigin是-250，remainingCacheExtent是1000，所以计算出的headerCacheExtent是0
-    // 再次假设我们scr0ll offset 是400
-    //  那么因为remainingPaintExtent是500，所以计算出的headerPaintExtent是0
-    //  那么因为cacheOrigin是-250，remainingCacheExtent是1000，所以计算出的headerCacheExtent是0
     double headerExtent = headerLogicalExtent;
     final double headerPaintExtent =
         calculatePaintOffset(constraints, from: 0, to: headerExtent);
@@ -191,7 +169,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
         calculateCacheOffset(constraints, from: 0, to: headerExtent);
 
     if (sliverChild == null) {
-      // 这里虽然没有传递所有的参数，但是满足了所有的基本需求
       geometry = SliverGeometry(
           scrollExtent: headerExtent,
           maxPaintExtent: headerExtent,
@@ -199,19 +176,11 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
           cacheExtent: headerCacheExtent,
           hitTestExtent: headerPaintExtent,
           hasVisualOverflow: headerExtent > headerPaintExtent);
-      //我认为hasVisualOverflow这样写，更准确，可以减少clip操作
-      // hasVisualOverflow: headerExtent > constraints.remainingPaintExtent || constraints.scrollOffset > 0);
     } else {
       sliverChild!.layout(
-        //这里五个参数，是必须的，其他的约束默认使用parent的约束，这里模拟的是一个去掉顶部高度的viewport约束
         constraints.copyWith(
           scrollOffset: math.max(0, constraints.scrollOffset - headerExtent),
           cacheOrigin: math.min(0, constraints.cacheOrigin + headerExtent),
-          //为什么重叠不是一个固定的值？
-          //因为headerExtent不为0的情况下，存在一个滚动后重叠的情况
-          //headerExtent为0的情况下，重叠一直是0
-          //为什么+一个值：这个我也没搞明白，我觉得第二行我写的是对的，为啥还有区分sticky，有点不懂
-          //  这个我暂时没法验证，除非我能构造一个上一个sliver突出到下一个sliver的case😂
           overlap: math.min(headerExtent, constraints.scrollOffset) +
               (sticky ? constraints.overlap : 0),
           // overlap: math.min(headerExtent, constraints.scrollOffset) + constraints.overlap),
@@ -224,7 +193,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
       );
       final SliverGeometry sliverChildLayoutGeometry = sliverChild!.geometry!;
       if (sliverChildLayoutGeometry.scrollOffsetCorrection != null) {
-        //这个scrollOffsetCorrection属性注释说的非常明白了，只要这个值不为0，会要求重新layout的，所以赋值后直接return
         geometry = SliverGeometry(
           scrollOffsetCorrection:
               sliverChildLayoutGeometry.scrollOffsetCorrection,
@@ -274,32 +242,16 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
       }
     }
 
-    //根据前面的scrollOffset校正，再次尝试矫正header的滚动距离
     if (headerChild != null) {
       final SliverPhysicalParentData? headerParentData =
           headerChild!.parentData as SliverPhysicalParentData?;
       final double sliverChildScrollExtent =
           sliverChild?.geometry?.scrollExtent ?? 0;
 
-      //这里之所以不允许constraints.scrollOffset + constraints.overlap) = 0的情况,是因为所有尚未滚动到顶部的sliver，一般情况下都是0
-      //这会导致所有header都想pin到viewport顶部，比如我们折叠的时候，会根据这个对列表执行jumpTo操作，就会受到干扰
-      //如果overlap > 0,那么第二个判断会一直为真，即便没有滚动到viewport顶部，
-      //只有title在顶部的时候，哪怕被推出去一部分，也算是pin，那么琢磨下这段代码：
-      //remainingPaintExtent代表剩余的extent，因为paintExtent可能大于layoutExtent，所以绘制可能到了外面。
-      // _isPinned = sticky &&
-      //     ((constraints.scrollOffset + constraints.overlap) > 0 ||
-      //         constraints.remainingPaintExtent == constraints.viewportMainAxisExtent);
-      // 我认为，只有sliver真正的开始滚动，并且还在viewport可见的情况下,pin才应该生效,
-      // 而且提前了这个计算，让下面的headerPosition计算使用这个flag
       _isPinned = sticky &&
           ((constraints.scrollOffset + constraints.overlap) > 0 &&
               geometry!.visible);
 
-      //为啥作者这里的计算是正确的，
-      //作者设计的效果是：当header不足一个高度时，header逐渐被推出屏幕
-      //对于overlapsContent=false的情况，滚动过整个childScrollExtent的时候，高度正好是0，然后就是负数了
-      //对于overlapsContent=true的情况，滚动过childScrollExtent - headerExtent的的时候，正好是0，然后就是负数了
-      //这里的position，是sliver体系的坐标, 从使用sticky改为使用_isPinned
       final double headerPosition = _isPinned
           ? math.min(
               constraints.overlap,
@@ -308,15 +260,12 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
                   (overlapsContent ? _headerExtent : 0))
           : -constraints.scrollOffset;
 
-      //     sticky ? math.min(constraints.overlap, scrollExtent - constraints.scrollOffset) : -constraints.scrollOffset;
-
       final double headerScrollRatio =
           ((headerPosition - constraints.overlap).abs() / _headerExtent);
       if (_isPinned && headerScrollRatio <= 1) {
         controller?.stickyCollapsablePanelScrollOffset =
             constraints.precedingScrollExtent;
       }
-      // second layout if scroll percentage changed and header is a RenderStickyCollapsablePanelLayoutBuilder.
       if (headerChild is RenderConstrainedLayoutBuilder<
           BoxValueConstraints<SliverStickyCollapsablePanelStatus>, RenderBox>) {
         double headerScrollRatioClamped = headerScrollRatio.clamp(0, 1.0);
@@ -402,7 +351,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
     return false;
   }
 
-  /// 主轴方向，相对ViewPort的leading edge的决定偏移量
   @override
   double childMainAxisPosition(RenderObject? child) {
     if (child == headerChild) {
@@ -415,8 +363,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
     return 0;
   }
 
-  /// 滚动偏移量，指的是需要滚动特定组件顶部所需要的距离
-  /// 比如header，就是0， sliver就是_headerExtent（当漂浮在item顶部的时候为0，否则为header的高度）
   @override
   double? childScrollOffset(RenderObject child) {
     assert(child.parent == this);
@@ -443,7 +389,6 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
         context.paintChild(sliverChild!, offset + childParentData.paintOffset);
       }
 
-      // The header must be drawn over the sliver, so draw it at last.
       if (headerChild != null) {
         final SliverPhysicalParentData headerParentData =
             headerChild!.parentData as SliverPhysicalParentData;
