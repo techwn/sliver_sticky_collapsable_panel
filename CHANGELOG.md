df0436c60b6605bd14bf69336dd884bbeccabe2b
Nannan Wang <nannan.wang@partner.bmw.com>
2023-11-23 10:26:06 +0800

    remove use of new api from flutter 3.13, and make sure flutter 2.5.0 compatibility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.8
+* remove use of new api from flutter 3.13, and make sure flutter 2.5.0 compatibility
+
 ## 1.0.7
 *  when expand pined header, calibration position more accurately
 
--- lib/rendering/render_sliver_sticky_collapsable_panel.dart
+++ lib/rendering/render_sliver_sticky_collapsable_panel.dart
@@ -303,8 +303,19 @@ class RenderSliverStickyCollapsablePanel extends RenderSliver
       }
 
       if (_iOSStyleSticky) {
-        geometry = geometry!
-            .copyWith(hitTestExtent: geometry!.hitTestExtent + headerExtent);
+        geometry = SliverGeometry(
+          scrollExtent: geometry!.scrollExtent,
+          paintExtent: geometry!.paintExtent,
+          paintOrigin: geometry!.paintOrigin,
+          layoutExtent: geometry!.layoutExtent,
+          maxPaintExtent: geometry!.maxPaintExtent,
+          maxScrollObstructionExtent: geometry!.maxScrollObstructionExtent,
+          hitTestExtent: geometry!.hitTestExtent + headerExtent,
+          visible: geometry!.visible,
+          hasVisualOverflow: geometry!.hasVisualOverflow,
+          scrollOffsetCorrection: geometry!.scrollOffsetCorrection,
+          cacheExtent: geometry!.cacheExtent,
+        );
       }
 
       switch (axisDirection) {
