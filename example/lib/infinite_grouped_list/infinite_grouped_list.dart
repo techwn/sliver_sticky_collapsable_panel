bd35dbb1a9019eae5044e4ae2bc6e5f294cc478f
noahwang <1215726657@qq.com>
2026-03-30 10:19:53 +0800

    use part to optimize the code file size

--- example/lib/infinite_grouped_list/infinite_grouped_list.dart
+++ example/lib/infinite_grouped_list/infinite_grouped_list.dart
@@ -545,7 +545,7 @@ class InfiniteGroupedListState<Cell, GroupBy, Group> extends State<InfiniteGroup
                               disableCollapsable: (index % 2) == 1,
                             ),
                           ),
-                          paddingAfterCollapse: const EdgeInsets.only(bottom: 10),
+                          // paddingAfterCollapse: const EdgeInsets.only(bottom: 10),
                           sticky: widget.stickyGroups,
                           expandCallback: (isExpanded) {
                             if (!isExpanded) {
